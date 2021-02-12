eaeeae356a878dc488a8380f8885c1f27dabb71d
ha6icht <rolf.karlen@gmail.com>
2021-02-12 18:21:09 +0100

    add starOuter[i].addEventListener

--- main.js
+++ main.js
@@ -19,26 +19,37 @@ const starOneValue = document.getElementById('star-1-value');
 const starOneNumber = document.getElementById('star-1-number');
 const avarageElement = document.getElementById('avarage');
 const totalNumber = document.getElementById('total-number');
-const five = 80;
-const four = 14;
-const three = 18;
-const two = 3;
-const one = 11;
-const sum = five+four+three+two+one;
-let avarage = (5*five+4*four+3*three+2*two+1*one)/sum;
-avarage = avarage.toFixed(1);
-starFiveValue.style.width = five/sum*100+'%';
-starFiveNumber.innerHTML = five;
-starFourValue.style.width = four/sum*100+'%';
-starFourNumber.innerHTML = four;
-starThreeValue.style.width = three/sum*100+'%';
-starThreeNumber.innerHTML = three;
-starTwoValue.style.width = two/sum*100+'%';
-starTwoNumber.innerHTML = two;
-starOneValue.style.width = one/sum*100+'%';
-starOneNumber.innerHTML = one;
-avarageElement.innerHTML = avarage;
-totalNumber.innerHTML = sum;
+let five = 80;
+let four = 14;
+let three = 18;
+let two = 3;
+let one = 11;
+let sum = 0;
+let avarage = 0;
+function calcValue(){
+    sum = five+four+three+two+one;
+    avarage = (5*five+4*four+3*three+2*two+1*one)/sum;
+    avarage = avarage.toFixed(1);
+    return avarage;
+}
+function calcChart(){
+    starFiveValue.style.width = five/sum*100+'%';
+    starFiveNumber.innerHTML = five;
+    starFourValue.style.width = four/sum*100+'%';
+    starFourNumber.innerHTML = four;
+    starThreeValue.style.width = three/sum*100+'%';
+    starThreeNumber.innerHTML = three;
+    starTwoValue.style.width = two/sum*100+'%';
+    starTwoNumber.innerHTML = two;
+    starOneValue.style.width = one/sum*100+'%';
+    starOneNumber.innerHTML = one;
+    avarageElement.innerHTML = avarage;
+    totalNumber.innerHTML = sum;
+}
+let starValue = calcValue();
+calcChart()
+getValue(starValue);
+console.log('main');
 for(let i = 0; i < starInner.length; i++){
     starInner[i].addEventListener('mouseover', () => {
         starInner1.style.width = '0';
@@ -46,17 +57,47 @@ for(let i = 0; i < starInner.length; i++){
         starInner3.style.width = '0';
         starInner4.style.width = '0';
         starInner5.style.width = '0';
+        starOuter[i].addEventListener('mouseleave', () =>{
+            getValue(starValue);
+        });
     });
     starOuter[i].addEventListener('click', (value) => {
         value = (i+1);
+        if(value === 1){
+            one++;
+            console.log('one: '+one);
+            starValue = calcValue();
+            calcChart();
+        } else if(value === 2){
+            two++;
+            console.log('two: '+two);
+            starValue = calcValue();
+            calcChart();
+        } else if(value === 3){
+            three++;
+            console.log('three: '+three);
+            starValue = calcValue();
+            calcChart();
+        } else if(value === 4){
+            four++;
+            console.log('four: '+four);
+            starValue = calcValue();
+            calcChart();
+        } else if(value === 5){
+            five++;
+            console.log('five: '+five);
+            starValue = calcValue();
+            calcChart();
+        }
         starOuter[i].addEventListener('mouseleave', () => {
-            getValue(value);
-        })
-    })
+            console.log('starValue: '+starValue);
+            getValue(starValue);
+        });
+    });
 }
 function getValue(value){
     console.log('value: '+ value);
-    console.log('inside getValue()');
+    //console.log('inside getValue()');
     let last;
     let check = true;
     const sub1 = Math.floor(value) * 100;
@@ -74,9 +115,9 @@ function getValue(value){
     if(sub1 === 0){
         starInner1.style.width = last+'%';
     } else if (sub1 === 100){
-        console.log('Inside else if(sub1 === 100)')
+        //console.log('Inside else if(sub1 === 100)')
         starInner1.style.width = '100%';
-        console.log(starInner1.style.width);
+        //console.log(starInner1.style.width);
         if(sub2 > 100 && check){
             starInner2.style.width = last+'%';
         }
